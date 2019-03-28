b813e8e71f88fcede3aec672f0aaf734ea77f4a2
218119 <218119@S020RMD114-07>
2019-03-28 10:05:06 -0500

    Commit I Finished

--- Block's Quest/Block's Quest/Block_s_Quest/Enemy.cs
+++ Block's Quest/Block's Quest/Block_s_Quest/Enemy.cs
@@ -10,39 +10,81 @@ namespace Block_s_Quest
 {
     class Enemy
     {
+        private Texture2D img;
         private int velocityX;
         private int velocityY;
         private Color color;
         private int hitpoints;
         private int size;
-        private 
-        public Enemy(int vX, int vY, Color col, int hitP, int s)
+        private Rectangle rect;
+        Boolean enemyMoveBegan;
+        Boolean isAlive = true;
+        Boolean spawnCoins;
+        public Enemy(Texture2D i, int vX, Color col, int hitP, int s, Rectangle r)
         {
+            img = i;
             velocityX = vX;
-            velocityY = vY;
             color = col;
             hitpoints = hitP;
             size = s;
+            rect = r;
         }
         public int getHitpoints()
         {
+            if (this.hitpoints <= 0)
+            {
+                isAlive = false;
+            }
+            else
+            {
+                isAlive = true;
+            }
             return hitpoints;
+        }
+        public void enemyKilled()
+        {
+           if (!isAlive)
+            {
+                spawnCoins = true;
+            }
         }
         public int getSize()
         {
             return size;
         }
-        public void ApplyPhysics()
+        private void ApplyPhysics()
         {
-
+            Double acceleration = -9.8;
+            velocityY = 0;
+            rect.X += velocityX;
+            rect.Y += velocityY;
+            if ((rect.X >= 0 || rect.X+rect.Width <= 1800) && !enemyMoveBegan)
+            {
+                enemyMoveBegan = true;
+                velocityY += (int)acceleration;
+            }
+            if (rect.Y+rect.Height >= 1000 || rect.Y <= 0)
+            {
+                velocityY *= -1;
+            }
+            if (enemyMoveBegan)
+            {
+                if (rect.X <= 0 || rect.X + rect.Width >= 1800)
+                {
+                    velocityX *= -1;
+                }
+            }
         }
         public void Update()
         {
-
+            ApplyPhysics();
         }
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-           
+            if (isAlive)
+            {
+                spriteBatch.Draw(img, rect, color);
+            }
         }
     }
 }
---
+++ Block's Quest/Block's Quest/Block_s_Quest/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Block_s_Quest
+{
+    class Level
+    {
+        public int levelIndex;
+        public List<Enemy> enemies;
+        public Level()
+        {
+
+        }
+    }
+}
