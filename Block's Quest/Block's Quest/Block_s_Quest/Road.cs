7fa4b73c562b8190bed0c77df340be4ff17a74fa
208011 <208011@S020RMD114-09>
2019-04-11 09:43:50 -0500

    RoAd

--- Block's Quest/Block's Quest/Block_s_Quest/Road.cs
+++ Block's Quest/Block's Quest/Block_s_Quest/Road.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Block_s_Quest
 {
@@ -14,5 +20,9 @@ namespace Block_s_Quest
             vertical
         };
 
+        public Road()
+        {
+
+        }
     }
 }
